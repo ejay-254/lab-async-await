4bbc7d55cf310677f8a19f845868e186f12d684c
ejay-254 <ellacherop@gmail.com>
2026-01-16 20:59:24 +0100

    run the tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(res => res.json())
+  .then(posts => displayPosts(posts));
+
+function displayPosts(posts) {
+  const ul = document.getElementById("post-list");
+
+  for (let post of posts) {
+    const li = document.createElement("li");
+    li.innerHTML = `<h1>${post.title}</h1><p>${post.body}</p>`;
+    ul.appendChild(li);
+  }
+}
